eb753c10f91f46dfb1769b717b84d7b77310bed1
Daniela Duncker <Daniela@Daniela-MacBook-Pro.local>
2019-04-01 13:26:54 +0300

    things

--- movie_website.py
+++ movie_website.py
@@ -1,1 +1,24 @@
-print("hello")
+import media
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "http://www.youtube.com/watch?v=vwyZH85NQC4")
+#print(toy_story.storyline)
+
+avatar = media.Movie("Avatar", "A marine on an alien planet", "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg", "http://www.youtube.com/watch?v=-9ceBgWV8io")
+#print(avatar.storyline)
+#avatar.show_trailer()
+
+harry_potter = media.Movie("Harry Potter and the Sorcerer's stone", "A boy finds out he's a wizard and goes to magic school", "http://upload.wikimedia.org/wikipedia/en/1/19/200px-HarryPotterAndThePhilosophersStoneMoviePoster.jpg", "https://www.youtube.com/watch?v=eKSB0gXl9dw")
+#print(harry_potter.storyline)
+#harry_potter.show_trailer()
+
+ratatouille = media.Movie("Ratatoouille", "A rat is a chef in Paris", "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg", "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+midnight_in_paris = media.Movie("Midnight in Paris", "Going back in time to meet authors", "http://upload.wikimedia.org/en/9/9f/Midnight_in_Paris_Poster.jpg", "https://youtube.com/watch?v=atLg2wQQxvU")
+
+school_of_rock = media.Movie("School of Rock", "Using rock music to learn", "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg", "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+
+
+
+
+
